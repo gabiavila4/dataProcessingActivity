bc668322076f1d418dd39efc1698c7ae14e8471a
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-10-20 10:28:21 -0500

    Initial Commit

---
+++ dataProcessingActivity/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dataProcessingActivity
+//
+//  Created by Gabriela Avila on 10/20/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
